7b654a34170fd5d7e4ae651d0398ccdab8394df8
Jan D <jan.dagefoerde@googlemail.com>
2020-10-02 09:44:03 +0200

    muffinbot

--- run-client.sh
+++ run-client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cobc -m -I src/copy/ src/DUMMBOT.CBL
+cobc -m -I src/copy/ src/MUFFINBO.CBL
 cobc -x -I src/copy src/CLIENT.CBL
-./CLIENT 192.168.42.220 9000 "Muffins" DUMMBOT
+./CLIENT 192.168.42.220 9000 "Muffins" MUFFINBO
